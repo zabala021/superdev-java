d47770024eea52b7ec8f4b6841b167ad5b6e3528
Vivo <wilianzabala014@gmail.com>
2024-03-01 22:22:37 -0300

    Estrutura de dados List

---
+++ HelloWorld/src/br/com/helloWorld/MainList.java
@@ -0,0 +1,43 @@
+package br.com.helloWorld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainList {
+
+	public static void main(String[] args) {
+		
+
+		List<String> listaAlunos = new ArrayList<String>();
+		
+		listaAlunos.add("Zeka");
+		listaAlunos.add("Jão");
+		listaAlunos.add("Tio Zé");
+		
+		System.out.println("Quantidade de alunos: " + listaAlunos.size());
+		
+		
+		
+		System.out.println("***Print FOR***");
+		for (int i = 0; i < listaAlunos.size(); i++) {
+			System.out.println(listaAlunos.get(i));
+		}
+		
+		System.out.println("***Print FOR..OF***");
+		for (String aluno: listaAlunos) {
+			System.out.println(aluno);
+		}
+		
+		Integer indexRemocao = listaAlunos.indexOf("Zeka");
+		if (indexRemocao >= 0) {
+			listaAlunos.remove(indexRemocao.intValue());
+		}
+		
+		System.out.println("***Print Lista Atualizada***");
+		for (String aluno: listaAlunos) {
+			System.out.println(aluno);
+		}
+
+	}
+
+}
